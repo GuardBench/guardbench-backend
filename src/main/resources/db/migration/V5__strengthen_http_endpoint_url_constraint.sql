02b16ccafd6074e3a5a4f06742d737650b35c8e2
Haein Choe <sodychoe@gmail.com>
2026-09-04 21:11:21 +0900

    refactor: remove superseded Flyway migration V5

--- src/main/resources/db/migration/V5__strengthen_http_endpoint_url_constraint.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE http_endpoint_target
-    DROP CONSTRAINT ck_http_endpoint_url_scheme,
-    ADD CONSTRAINT ck_http_endpoint_url_scheme
-        CHECK (endpoint_url ~* '^https?://[^/?#[:space:]]+');
